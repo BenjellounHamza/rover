b66ebc09d1d29261c49d4c95fbbecd4200da30e5
BenjellounHamza <hmzbnjlln@gmail.com>
2022-01-17 10:14:03 +0100

    cleaning

--- rover/src/main/java/utils/InputChecker.java
+++ rover/src/main/java/utils/InputChecker.java
@@ -8,12 +8,12 @@ public class InputChecker {
 
 	
 	static private final Set<String> possibleDirections = new HashSet<>(Arrays.asList("N", "E", "S", "W"));
-	static private final Set<Character> possibleCommands = new HashSet<>(Arrays.asList('L', 'R', 'M'));
+	static private final Set<String> possibleCommands = new HashSet<>(Arrays.asList("L", "R", "M"));
 	
 	
 	public static void checkPlateau(String line) throws IllegalArgumentException{
 		String[] plateauDimension = line.split(" ");
-		if(plateauDimension.length < 2) {
+		if(plateauDimension.length != 2) {
             throw new IllegalArgumentException("The plateau shoud have 2 coordinates");
 		}
         if (Integer.parseInt(plateauDimension[0]) <= 0 || Integer.parseInt(plateauDimension[1]) <= 0) {
@@ -25,19 +25,19 @@ public class InputChecker {
 	public static void checkRover(String line, Plateau plateau) throws IllegalArgumentException {
 		String[] roverPosition = line.split(" ");
 		
-		if(roverPosition.length < 3) {
+		if(roverPosition.length != 3) {
             throw new IllegalArgumentException("The rover shoud have 2 coordinates and one direction");
 		}
 		if (Integer.parseInt(roverPosition[0]) > plateau.getX() || Integer.parseInt(roverPosition[1]) > plateau.getY()) {
 	           throw new IllegalArgumentException("The rover's coordinates must be less than plateau's ones");
 	 }
-		if (roverPosition[2].isEmpty() || !possibleDirections.contains(roverPosition[2])) {
+		if (!possibleDirections.contains(roverPosition[2])) {
 	           throw new IllegalArgumentException("The rover's direction must be N, E, S or W");
 	 }
 	}
 
 	public static void checkCommands(String line) throws IllegalArgumentException{
-		for (char command: line.toCharArray()) {
+		for (String command: line.split("")) {
             if(!possibleCommands.contains(command)) {
  	           throw new IllegalArgumentException("The rover's commands must be either M, L or R");
             }
